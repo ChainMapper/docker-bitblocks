ff5e856ca5faac7f5b8f7b47137c29e65c13abbc
Jos Hendriks <jos@hendriks.software>
2019-02-06 21:41:27 +0100

    Added new nodes

--- gen_config.sh
+++ gen_config.sh
@@ -7,7 +7,14 @@ rpcallowip=$RPCALLOW
 rpcport=6666
 txindex=1
 maxconnetions=50
-connect=31.220.57.53
-connect=31.220.57.56
-connect=31.220.57.61
+addnode=s1.bitblocksproject.com
+addnode=s2.bitblocksproject.com
+addnode=s3.bitblocksproject.com
+addnode=s4.bitblocksproject.com
+addnode=s5.bitblocksproject.com
+addnode=s6.bitblocksproject.com
+addnode=s7.bitblocksproject.com
+addnode=s8.bitblocksproject.com
+addnode=s9.bitblocksproject.com
+addnode=s10.bitblocksproject.com
 EOF
